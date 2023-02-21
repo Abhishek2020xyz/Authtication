d9b50a33e3d9bcf6878155249612e0e471e67ec9
Abhishek2020xyz <116792007+Abhishek2020xyz@users.noreply.github.com>
2023-02-21 09:25:34 +0530

    Add files via upload

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,9 +1,11 @@
 import { useRef, useContext } from 'react'; 
+import { useHistory } from 'react-router-dom';
 import AuthContext from '../../store/auth-context';
  
  import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+const history = useHistory();
 const newPasswordInputRef = useRef();
 const authCtx = useContext(AuthContext);
 const submitHandler = (event) => {
@@ -24,6 +26,7 @@ const submitHandler = (event) => {
       'Content-Type': 'application/json'
     }
   }).then(res =>{
+    history.replace('/');
     
   })
 }
